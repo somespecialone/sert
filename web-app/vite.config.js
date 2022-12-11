ad06af0fc3d752ea0b6055bed9cdf3c67998ae89
somespecialone <tkachenkodmitriy@yahoo.com>
2022-12-11 23:21:56 +0100

    Move to custom domain

--- web-app/vite.config.js
+++ web-app/vite.config.js
@@ -5,7 +5,6 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
-  base: "/steam-exchange-rate-tracker/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
