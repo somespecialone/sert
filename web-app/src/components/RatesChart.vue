181d683739941a895edc98f6cdc9e674e5182e2f
somespecialone <tkachenkodmitriy@yahoo.com>
2022-12-17 14:09:43 +0100

    More readable graph.

--- web-app/src/components/RatesChart.vue
+++ web-app/src/components/RatesChart.vue
@@ -1,22 +1,46 @@
 <script>
 import { Line } from "vue-chartjs";
 import { CategoryScale, Chart as ChartJS, LinearScale, LineElement, PointElement, Tooltip } from "chart.js";
+import ChartDataLabels from "chartjs-plugin-datalabels";
 
 import { myRound } from "@/utils";
 import { COLORS } from "@/constants";
 
-ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Tooltip);
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Tooltip, ChartDataLabels);
 
 const chartOptions = {
-  elements: { line: { tension: 0.4 } },
+  elements: {
+    line: { tension: 0.4 },
+    point: { pointStyle: "rectRounded", radius: 4, hoverRadius: 6, hitRadius: 11 },
+  },
   maintainAspectRatio: false,
+  layout: {
+    padding: {
+      top: 12,
+      right: 20,
+      bottom: 12,
+      left: 20,
+    },
+  },
   plugins: {
+    datalabels: {
+      backgroundColor(ctx) {
+        return ctx.dataset.backgroundColor;
+      },
+      borderRadius: 4,
+      color: "white",
+      formatter(value, ctx) {
+        return ctx.dataset.data[ctx.dataIndex].rate;
+      },
+      padding: 4,
+      display: "auto",
+    },
     tooltip: {
       callbacks: {
         label(ctx) {
           const percentage = (ctx.raw.y > 0 ? "+" : "") + ctx.raw.y * 100;
           const points = (ctx.raw.changePoints > 0 ? "+" : "") + ctx.raw.changePoints;
-          return `${ctx.dataset.label}: ${ctx.raw.rate} | ${percentage}% | ${points}`;
+          return ` ${ctx.dataset.label}: ${ctx.raw.rate} | ${percentage}% | ${points}`;
         },
       },
     },
@@ -26,7 +50,10 @@ const chartOptions = {
       reverse: true,
       display: false,
     },
-    y: { display: false },
+    y: {
+      reverse: true,
+      display: false,
+    },
   },
 };
 
@@ -55,13 +82,8 @@ export default {
           backgroundColor: COLORS[currKey],
           borderColor: COLORS[currKey],
           data: entries.reduce((newEntryArr, [rate, ts]) => {
-            if (newEntryArr.length) {
-              const prevRate = (newEntryArr[newEntryArr.length - 1] || { rate }).rate;
-              const [y, changePoints] = this.calcChangePercentPoints(prevRate, rate);
-              newEntryArr[newEntryArr.length - 1].y = y;
-              newEntryArr[newEntryArr.length - 1].changePoints = changePoints;
-            }
-            newEntryArr.push({ y: 0, x: new Date(ts * 1000).toDateString(), rate, changePoints: 0 });
+            const [y, changePoints] = this.calcChangePercentPoints(entries[entries.length - 1][0], rate);
+            newEntryArr.push({ y, x: new Date(ts * 1000).toDateString(), rate, changePoints });
             return newEntryArr;
           }, []),
         });
